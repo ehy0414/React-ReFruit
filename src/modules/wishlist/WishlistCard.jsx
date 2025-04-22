eed5fd494a25024f5295476ee290fd60c0bbc404
ehy0414 <djagusdyd78@gmail.com>
2025-04-22 12:46:15 +0900

    :art: design: css 수정

--- src/modules/wishlist/WishlistCard.jsx
+++ src/modules/wishlist/WishlistCard.jsx
@@ -5,71 +5,73 @@ import { CartButton } from "./CartButton";
 import { DeleteIcon } from "./Icons";
 
 const Card = styled.article`
-  width: 235px;
-  border-radius: 4px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 12px;
-  position: relative;
-  background-color: #f5f5f5;
+    width: 235px;
+    border-radius: 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 12px;
+    position: relative;
+    background-color: #f5f5f5;
 
-  @media (max-width: 991px) {
-    width: 45%;
-  }
+    @media (max-width: 991px) {
+        width: 45%;
+    }
 
-  @media (max-width: 640px) {
-    width: 100%;
-  }
+    @media (max-width: 640px) {
+        width: 100%;
+    }
 `;
 
 
 const ImageContainer = styled.div`
-  position: relative;
-  width: 86%;
-  height: 150px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 12px;
+    position: relative;
+    width: 86%;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
 `;
 
 
 const ProductImage = styled.img`
-  max-width: 80%;
-  max-height: 80%;
-  margin-left: 5%;
-  margin-top: 12%;
-  border-radius: 4px;
+    max-width: 80%;
+    max-height: 80%;
+    margin-left: 5%;
+    margin-top: 12%;
+    border-radius: 4px;
 `;
 
 const DiscountBadge = styled.div`
-  position: absolute;
-  top: 12px;
-  left: 12px;
-  color: #fafafa;
-  padding: 4px 12px;
-  border-radius: 4px;
-  font-family: "Poppins", sans-serif;
-  font-size: 12px;
-  background-color: #db4444;
+    position: absolute;
+    top: 12px;
+    left: 12px;
+    color: #fafafa;
+    padding: 4px 12px;
+    border-radius: 4px;
+    font-family: "Poppins", sans-serif;
+    font-size: 12px;
+    background-color: #db4444;
 `;
 
 const ProductInfo = styled.div`
-  padding: 0 16px;
+    padding: 0 16px;
 `;
 
 const ProductName = styled.h3`
-  font-family: "Poppins", sans-serif;
-  font-size: 16px;
-  color: #000;
-  font-weight: 500;
-  margin: 0;
+    font-family: "Poppins", sans-serif;
+    font-size: 16px;
+    color: #000;
+    font-weight: 500;
+    margin-top: -5px;
 `;
 
 const PriceContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-top: -16px;
+  margin-bottom: 10px;
   gap: 12px;
 `;
 
@@ -109,7 +111,7 @@ export const WishlistCard = ({
       </ImageContainer>
       <CartButton onClick={onAddToCart} />
       <ProductInfo>
-        <ProductName>{name}</ProductName>
+        <ProductName>{name}sadasdasdsadsa</ProductName>
         <PriceContainer>
           <Price>${price}</Price>
           {originalPrice && <OriginalPrice>${originalPrice}</OriginalPrice>}
